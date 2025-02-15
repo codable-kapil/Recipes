f1cb661b111d1b476358d94b659ff3a76d95469c
codable-kapil <kapil.shivhare09@gmail.com>
2025-02-15 12:31:42 -0800

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Kapil Shivhare on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
